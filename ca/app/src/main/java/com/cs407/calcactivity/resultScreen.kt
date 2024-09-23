49608ab265c1af1c5f92d22b9f1d78f6b8080cab
lucybutler <lucypbutler@gmail.com>
2024-09-23 15:48:32 -0500

    Commit everything

---
+++ ca/app/src/main/java/com/cs407/calcactivity/resultScreen.kt
@@ -0,0 +1,30 @@
+package com.cs407.calcactivity
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class resultScreen : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_result_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //Retrieve the data passed from MainActivity
+        val message = intent.getStringExtra("Result: ")
+
+        //Find the text view and display the data
+       findViewById<TextView>(R.id.resultTextView).text = "Result: "+ message.toString()
+
+    }
+}
